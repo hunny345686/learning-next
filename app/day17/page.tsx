d0bdab96c26bc9586c6adc776fc81617602d9a20
ipremsingh <prem.singh@oodlestechnologies.com>
2026-02-28 08:40:38 +0530

    Leanrn about prizma how to talk to real DB

---
+++ app/day17/page.tsx
@@ -0,0 +1,16 @@
+
+type Product = {
+    id: number,
+    title: string,
+    price: number,
+    desc: string | null
+}
+export default function Day17() {
+    return (
+        <div>
+            <h2>Day 17 Fetching data from DB</h2>
+        </div>
+
+    )
+}
+
